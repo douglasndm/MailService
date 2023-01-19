68992e544834d72e79ce4331e9b8916b03299b7b
Douglas Nunes de Mattos <7841509+douglasndm@users.noreply.github.com>
2023-01-19 20:56:23 -0300

    Adding route to send confirmation email

---
+++ src/App/Controllers/AccountConfirmation.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from 'express';
+import path from 'path';
+import fs from 'fs';
+import mjml from 'mjml';
+import { compile } from 'handlebars';
+import * as Yup from 'yup';
+
+import Mail from '@services/Mail';
+
+import AppError from '@errors/AppError';
+
+class AccountConfirmationController {
+    async store(req: Request, res: Response): Promise<Response> {
+        const schema = Yup.object().shape({
+            to: Yup.string().required().email('Invalid email'),
+            subject: Yup.string().required(),
+            name: Yup.string().required(),
+            AppName: Yup.string().required(),
+            manager: Yup.string().required(),
+            team: Yup.string().required(),
+            confirmationLink: Yup.string().url().required(),
+        });
+
+        try {
+            await schema.validate(req.body);
+        } catch (err) {
+            if (err instanceof Error)
+                throw new AppError({ message: err.message });
+        }
+
+        const templatesFolder = path.resolve(
+            __dirname,
+            '..',
+            '..',
+            'Mail',
+            'Templates',
+            'ExpiryTeams',
+        );
+
+        const templatePath = path.resolve(
+            templatesFolder,
+            'EmailAccountConfirmation.mjml',
+        );
+
+        const templateFileContent = await fs.promises.readFile(templatePath, {
+            encoding: 'utf-8',
+        });
+
+        const baseHtml = mjml(templateFileContent).html;
+        const compiledTemplate = compile(baseHtml);
+        const final = compiledTemplate(req.body);
+
+        Mail.sendMail({
+            from: {
+                address: process.env.MAIL_USER || 'noreplay@douglasndm.dev',
+                name: req.body.AppName,
+            },
+            to: req.body.to,
+            subject: req.body.subject,
+            html: final,
+        });
+
+        return res.status(201).send();
+    }
+}
+
+export default new AccountConfirmationController();
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import MailController from '@controllers/Mail';
+import AccountConfirmation from '@controllers/AccountConfirmation';
 
 const routes = Router();
 
 routes.post('/send', MailController.store);
+routes.post('/account/confirmation', AccountConfirmation.store);
 
 export default routes;
